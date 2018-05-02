ead2d5696026bc9f5ea647de929bcd35f454cb6f
glushchenko-aleksandr <glushchenko.aleksandr@gmail.com>
2018-05-02 12:51:09 +0300

    put placeholders for hostname and password

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ var config = {};
 config.servicenow = {};
 
 config.servicenow.username = 'admin';
-config.servicenow.password = 'UjAgl5syKXU6';
-config.servicenow.hostname = 'dev57441.service-now.com';
+config.servicenow.password = 'your-servicenow-password';
+config.servicenow.hostname = 'your-servicenow-host';
 config.servicenow.port = 443;
 
 module.exports = config;
